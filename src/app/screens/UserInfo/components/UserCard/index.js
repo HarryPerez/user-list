31b04950402ae006e01187262801fb367716646c
Franco Perez <36163179+HarryPerez@users.noreply.github.com>
2021-07-07 19:49:14 -0300

    finish mvp with user list with all data

--- src/app/screens/UserInfo/components/UserCard/index.js
+++ src/app/screens/UserInfo/components/UserCard/index.js
@@ -4,9 +4,30 @@ import "./styles.css";
 
 function UserCard({ user }) {
   const userName = `${user.name.title} ${user.name.first} ${user.name.last}`;
+  const location = `${user.location.city} ${user.location.state} ${user.location.country}`;
+  const { picture, email } = user;
+
+  console.log(user);
+
   return (
     <div className="user-card-container">
-      <div>{userName}</div>
+      <div className="user-card-header">
+        <div className="user-name">{userName}</div>
+      </div>
+      <div className="user-card-body">
+        <div className="user-pic-container">
+          <img
+            className="user-pic"
+            alt={`user-pic${user.id.value}`}
+            src={picture.large}
+          />
+        </div>
+        <div className="user-description">
+          <div className="user-text">{email}</div>
+          <div className="user-text">{userName}</div>
+          <div className="user-text">{userName}</div>
+        </div>
+      </div>
     </div>
   );
 }
--- src/app/screens/UserInfo/index.js
+++ src/app/screens/UserInfo/index.js
@@ -1,14 +1,23 @@
-import React from 'react';
-import { useQuery } from 'react-query';
+import React from "react";
+import { useQuery } from "react-query";
 
-import { getUsers } from '../../../services/UserService';
+import { getUsers } from "../../../services/UserService";
 
-import UserCard from './components/UserCard';
+import UserCard from "./components/UserCard";
+
+import "./styles.css";
 
 function UserInfo() {
-  const { data } = useQuery('users', getUsers);
-  const users = data?.data?.results || [];
-  return users.map(user => <UserCard user={user} />);
+  const { data } = useQuery("users", getUsers);
+  const users = data?.data.results || [];
+
+  return (
+    <div className="user-info-container">
+      {users.map(
+        (user) => user.id.value && <UserCard key={user.id.value} user={user} />
+      )}
+    </div>
+  );
 }
 
 export default UserInfo;
