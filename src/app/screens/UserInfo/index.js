12823846e5e4a5a90554a336daa32c8655250009
Franco Perez <36163179+HarryPerez@users.noreply.github.com>
2021-07-07 20:18:12 -0300

    add filter

--- src/app/screens/UserInfo/index.js
+++ src/app/screens/UserInfo/index.js
@@ -6,6 +6,8 @@ import { getUsers } from "../../../services/UserService";
 import SearchBar from "./components/SearchBar";
 import UserCard from "./components/UserCard";
 
+import { getUserInfo } from "./utils";
+
 import "./styles.css";
 
 function UserInfo() {
@@ -15,11 +17,24 @@ function UserInfo() {
 
   const handleSetFilter = (value) => setFilter(value);
 
-  console.log(filter);
+  const filterUserByAll = () =>
+    users.filter((usr) => {
+      const userValues = getUserInfo(usr);
+      const hasAnyMatch = Object.values(userValues).find((value) => {
+        if (typeof value !== "object") {
+          return value.toLowerCase().includes(filter.toLowerCase());
+        }
+        return false;
+      });
+      return hasAnyMatch;
+    });
+
+  const filteredUsers = filter !== "" ? filterUserByAll() : users;
+
   return (
     <div className="user-info-container">
       <SearchBar onHandleChange={handleSetFilter} />
-      {users.map(
+      {filteredUsers?.map(
         (user) => user.id.value && <UserCard key={user.id.value} user={user} />
       )}
     </div>
